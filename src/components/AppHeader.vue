8a813e71348bc984f06136860ca7cf138722f306
martinaapalazzi <martinaapalazzi@gmail.com>
2024-01-18 15:47:44 +0100

    edit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,39 @@
   export default {
     data() {
       return {
-        
+        linksHeader: [
+          {
+            link: 'CHARACTERS'
+          },
+          {
+            link: 'COMICS'
+          },
+          {
+            link: 'MOVIES'
+          },
+          {
+            link: 'TV'
+          },
+          {
+            link: 'GAMES'
+          },
+          {
+            link: 'COLLECTIBLES'
+          },
+          {
+            link: 'VIDEOS'
+          },
+          {
+            link: 'FANS'
+          },
+          {
+            link: 'NEWS'
+          },
+          {
+            link: 'SHOP'
+          },
+
+        ]
       }
     },
   }
@@ -10,8 +42,20 @@
 
 <template>
 
-  <div>
-    <p>HEADER</p>
+  <div id="header">
+
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="(singleLinkHeader, index) in linksHeader">
+          <a href="#"> {{ singleLinkHeader.link }}</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
   
 </template>
