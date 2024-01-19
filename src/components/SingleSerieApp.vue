9b7bf53d5eba938288bff210cadc3df4b7f7beeb
martinaapalazzi <martinaapalazzi@gmail.com>
2024-01-19 14:19:43 +0100

    added jumbotron + created singleserie.vue

---
+++ src/components/SingleSerieApp.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+      return {
+
+      }
+    }
+}
+
+
+</script>
+
+<template>
+
+  <div>
+    SERIE
+  </div>
+  
+</template>
+
+<style lang="scss">
+
+</style>
